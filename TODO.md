c6f1370a7116123f896f7e42ffd4919fcd43429c
anthonytranDev <anthonytran.dev@gmail.com>
2020-07-27 13:25:36 +0100

    routing section completed

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# To Do
+
+## Post Tutorial
+
+- Deconstruct entire project app (recommended per commit)
+  - Note down maps and notes
+- Include tests
+- Include TypeScript into the mix
--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ApolloProvider } from 'react-apollo'
-import { ApolloClient } from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 
-import 'tachyons/css/tachyons.min.css'
-import './styles/index.css';
+import "tachyons/css/tachyons.min.css";
+import "./styles/index.css";
+import { BrowserRouter } from "react-router-dom";
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
-})
+  uri: "http://localhost:4000",
+});
 
 const client = new ApolloClient({
   link: httpLink,
-  cache: new InMemoryCache()
-})
-
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root')
-)
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
