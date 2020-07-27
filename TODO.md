b14cff1f443343f70e7bd6580cbf923a6621ccda
anthonytranDev <anthonytran.dev@gmail.com>
2020-07-27 14:58:03 +0100

    authentication section completed

--- TODO.md
+++ TODO.md
@@ -4,5 +4,6 @@
 
 - Deconstruct entire project app (recommended per commit)
   - Note down maps and notes
+  - Search for specific languaged words
 - Include tests
 - Include TypeScript into the mix
--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
-import { ApolloClient } from "apollo-client";
-import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { setContext } from "apollo-link-context";
+import { createHttpLink } from "apollo-link-http";
 
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
+import { AUTH_TOKEN } from "./constants";
+
 import "tachyons/css/tachyons.min.css";
 import "./styles/index.css";
-import { BrowserRouter } from "react-router-dom";
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000",
 });
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
