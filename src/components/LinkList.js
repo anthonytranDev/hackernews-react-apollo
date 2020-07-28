3bba3d0d36146cf361bfa963f8987e4adc4ff684
anthonytranDev <anthonytran.dev@gmail.com>
2020-07-28 13:41:32 +0100

    style prettier link list

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
-import { Query } from 'react-apollo'
-import gql from 'graphql-tag'
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
-import Link from './Link'
+import Link from "./Link";
 
 export const FEED_QUERY = gql`
-  {
+  query {
     feed {
       links {
         id
@@ -25,17 +25,17 @@ export const FEED_QUERY = gql`
       }
     }
   }
-`
+`;
 
 class LinkList extends Component {
   render() {
     return (
       <Query query={FEED_QUERY}>
         {({ loading, error, data }) => {
-          if (loading) return <div>Fetching</div>
-          if (error) return <div>Error</div>
+          if (loading) return <div>Fetching</div>;
+          if (error) return <div>Error</div>;
 
-          const linksToRender = data.feed.links
+          const linksToRender = data.feed.links;
 
           return (
             <div>
@@ -48,20 +48,20 @@ class LinkList extends Component {
                 />
               ))}
             </div>
-          )
+          );
         }}
       </Query>
-    )
+    );
   }
 
   _updateCacheAfterVote = (store, createVote, linkId) => {
-    const data = store.readQuery({ query: FEED_QUERY })
+    const data = store.readQuery({ query: FEED_QUERY });
 
-    const votedLink = data.feed.links.find(link => link.id === linkId)
-    votedLink.votes = createVote.link.votes
+    const votedLink = data.feed.links.find((link) => link.id === linkId);
+    votedLink.votes = createVote.link.votes;
 
-    store.writeQuery({ query: FEED_QUERY, data })
-  }
+    store.writeQuery({ query: FEED_QUERY, data });
+  };
 }
 
-export default LinkList
+export default LinkList;
